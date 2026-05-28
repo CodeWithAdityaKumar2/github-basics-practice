0adba8779d04286b6f86886db86e427657314e60
Aditya Kumar <adityakumar7079629108@gmail.com>
2026-05-28 19:33:26 +0530

    Added login feature

---
+++ app.js
@@ -0,0 +1,11 @@
+// added login feature
+function login(username, password) {
+    // logic to authenticate user
+    console.log("User logged in with username: " + username);
+}
+
+// added logout feature
+function logout() {
+    // logic to log out user
+    console.log("User logged out");
+}
